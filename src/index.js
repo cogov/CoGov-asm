15df8658fceaa34f1b53aff9c0078c20d92e3042
Immortal <Immortal@Tanujs-MacBook-Pro.local>
2019-04-17 00:32:36 +0530

    fixed warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
